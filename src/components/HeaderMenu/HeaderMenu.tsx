d90273b63ac7a0062b128e520a7c36348ba8ffae
elena.sestrasound <elena.sestrasound@gmail.com>
2023-02-14 12:31:25 +0100

    declaring firsts components

---
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IHeaderMenuProps {
+}
+
+export default function HeaderMenu(props: IHeaderMenuProps) {
+    return (
+        <div>
+
+        </div>
+    );
+}
