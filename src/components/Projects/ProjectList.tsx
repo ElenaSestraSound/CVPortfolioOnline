11da804d14cc2233e6578d63be471a41adb2807d
elena.sestrasound <elena.sestrasound@gmail.com>
2023-03-08 12:30:47 +0100

    added pictures for projects

--- src/components/Projects/ProjectList.tsx
+++ src/components/Projects/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { ProjectDetails } from "@/lib/types";
 import portfolioImg from '@/assets/Projects/portfolio.png'
+import foodappImg from '@/assets/Projects/foodapp.png'
 import NextLogo from '@/assets/tecnologies/#070707/next.svg'
 import TypeLogo from '@/assets/tecnologies/#070707/tys.svg'
 import ChakraLogo from '@/assets/tecnologies/#070707/chakra.svg'
@@ -10,7 +11,7 @@ import ReactLogo from '@/assets/tecnologies/#070707/react.svg'
 export const ProjectList: ProjectDetails[] = [
     {
         name: 'Portfolio CV online',
-        description: 'This is my portfolio and CV website, where I have fetured my resumee and projects',
+        description: 'This is my portfolio and CV website, where I have fetured my resumee and projects.',
         links:
             [{ name: 'Portfolio CV url', to: '#' },
             { name: 'GitHub Repository', to: '#' }],
@@ -23,7 +24,7 @@ export const ProjectList: ProjectDetails[] = [
         links:
             [{ name: 'Order Food App url', to: '#' },
             { name: 'GitHub Repository', to: 'https://github.com/ElenaSestraSound/order-food-app' }],
-        image: portfolioImg,
+        image: foodappImg,
         tecnologies: [<ReactLogo />, <TypeLogo />, <ChakraLogo />]
     },
     // {
