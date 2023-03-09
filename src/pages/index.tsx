6eb44ed7f9ba9d402353ab95b83b37bb1db1bbf3
elena.sestrasound <elena.sestrasound@gmail.com>
2023-03-09 11:32:42 +0100

    deleted unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,9 @@
 import Layout from '@/components/Layout'
-import { navigationActions } from '@/store/navigation-slice';
 import { SECTIONS } from '@/utils/CommonUtils'
 import Head from 'next/head';
-import { useEffect, useRef } from 'react';
-import { useInViewport } from 'react-in-viewport';
-import { useDispatch } from 'react-redux';
 import { v4 as uuid_v4 } from 'uuid'
 
 export default function Home() {
-  // const ref = useRef<HTMLDivElement>(null)
-  // const options = { threshold: 0.7 }
-  // const { inViewport } = useInViewport(ref, options);
-
-  // const dispatch = useDispatch()
-
-  // useEffect(() => {
-  //   if (inViewport) {
-  //     if (ref.current) {
-  //       console.log(ref.current.id + " is in viewPort")
-  //       dispatch(navigationActions.setActiveSection(ref.current.id))
-  //     }
-  //   }
-  // }, [inViewport, dispatch, ref])
   return (
     <>
       <Head>
