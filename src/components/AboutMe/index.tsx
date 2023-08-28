11cf85668c895c18f3d28e034231e082989836c7
elena.sestrasound <elena.sestrasound@gmail.com>
2023-08-28 11:20:35 +0200

    chore: updated data

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,54 +1,54 @@
 import { Heading, Text, Box, Flex, Center } from '@chakra-ui/react';
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { ChevronRightIcon } from '@chakra-ui/icons';
 import * as React from 'react';
-import * as styles from './styles'
+import * as styles from './styles';
 import Section from '../DesignSystem/Section';
 import Image from 'next/image';
-import pic from '@/assets/rocketEmail.png'
+import pic from '@/assets/rocketEmail.png';
 import SecondaryLink from '../DesignSystem/Links/SecondaryLink';
 import useViewport from '@/hooks/use-viewport';
 
 
 const AboutMe = () => {
-    const ref = React.useRef<HTMLDivElement>(null)
-    useViewport(ref)
-    return (
-        <Section bg='brand.backgroundSecondary' id='about-me' ref={ref}>
-            <Flex css={styles.aboutMeContainer}>
-                <Box css={styles.detailsBox}>
-                    <Heading as='h2' size='xl' color='brand.textPrimary'>
-                        A little bit about me
-                    </Heading>
-                    <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
-                        I am a web developer with experience working in various digital newspapers and magazines as a frontend developer.
-                        While I enjoyed my work, I often found myself feeling stuck using the same old technologies repeatedly.
-                        I realized that I needed to level up my knowledge to keep up with the ever-evolving digital landscape.
-                    </Text>
-                    <br />
-                    <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
-                        Now, I specialize in developing modern web applications using Nextjs, React, and Typescript.
-                        My goal is to continue learning and expanding my knowledge to gain full-stack abilities, allowing me to have an overview of how everything works together.
-                        I'm also passionate about sharing my knowledge with others, which is why I have started writing a blog about coding to help others solve the same problems I faced.
-                        By keeping up with the latest technologies and sharing my experiences, I believe I can make a positive impact on the coding community.
-                    </Text>
-                    <br />
-                    <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
-                        In addition to my passion for coding, I have a deep love for audiovisuals and creating content.
-                        I've studied music production and video editing, which has given me a unique perspective on creating engaging and high-quality content.
-                        I believe that by combining my technical knowledge and creative skills, I can create innovative and effective solutions for my clients.
-                    </Text>
-                    <br />
-                    <SecondaryLink to='projects' isExternal={false} > Checkout my React projects here<ChevronRightIcon /></SecondaryLink>
-                </Box>
-                <Center css={styles.imageBox}>
-                    <Image
-                        src={pic}
-                        alt="Picture of the author"
-                    />
-                </Center>
-            </Flex>
-        </Section>
-    );
-}
+  const ref = React.useRef<HTMLDivElement>(null);
+  useViewport(ref);
+  return (
+    <Section bg='brand.backgroundSecondary' id='about-me' ref={ref}>
+      <Flex css={styles.aboutMeContainer}>
+        <Box css={styles.detailsBox}>
+          <Heading as='h2' size='xl' color='brand.textPrimary'>
+            A little bit about me
+          </Heading>
+          <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
+            I am a web developer with experience working in various digital newspapers and magazines as a frontend developer.
+            While I enjoyed my work, I often found myself feeling stuck using the same old technologies repeatedly.
+            I realized that I needed to level up my knowledge to keep up with the ever-evolving digital landscape.
+          </Text>
+          <br />
+          <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
+            Now, I'm a full-stack developer that specializes in developing modern web applications using Nextjs, React, and Typescript, with knowledge
+            on the backend side with Node and Express.
+            I'm also passionate about sharing my knowledge with others, which is why I have started writing a blog about coding to help others solve the same problems I faced.
+            By keeping up with the latest technologies and sharing my experiences, I believe I can make a positive impact on the coding community.
+          </Text>
+          <br />
+          <Text fontSize='2xl' fontWeight={600} color='brand.textSecondary'>
+            In addition to my passion for coding, I have a deep love for audiovisuals and creating content.
+            I've studied music production and video editing, which has given me a unique perspective on creating engaging and high-quality content.
+            I believe that by combining my technical knowledge and creative skills, I can create innovative and effective solutions for my clients.
+          </Text>
+          <br />
+          <SecondaryLink to='projects' isExternal={false} > Checkout my React projects here<ChevronRightIcon /></SecondaryLink>
+        </Box>
+        <Center css={styles.imageBox}>
+          <Image
+            src={pic}
+            alt="Picture of the author"
+          />
+        </Center>
+      </Flex>
+    </Section>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/Resumee/ResumeeTable/TableContentData.ts
+++ src/components/Resumee/ResumeeTable/TableContentData.ts
@@ -1,47 +1,52 @@
 import { TableContentDetail } from "@/lib/types";
 
 export const Jobs: TableContentDetail[] = [
-    {
-        title: 'FRONTED DEVELOPER',
-        place: 'Asesoría Técnica Editorial, Madrid, Spain',
-        description: 'ATE is an agency that provides web development services to magazines: Developed various websites using wordpress, JavaScript, SCSS and HTML. Maintenance and customer support.',
-        date: 'Nov 2020 - Dec 2022'
-    },
-    {
-        title: 'FRONTEND DEVELOPER',
-        place: 'El Independiente - Madrid, Spain',
-        description: 'El Independiente is a popular digital spanish newspaper. Developed fronted features like sliders, timelines, news feed, etc. using: JavaScript, SCSS and HTML.Developed Wordpress internal tools to help journalists using: JavaScript, SCSS, HTML and PHP',
-        date: 'Feb 2018 - Oct 2020'
-    },
-    {
-        title: 'FRONTEND DEVELOPER',
-        place: 'El Español- Madrid, Spain',
-        description: "El Español is a popular digital spanish newspaper. Created the layout for different pages and widgets, like the Olympics 2016 special or the newspaper's header redesign, using SCSS and HTMLDeveloped behavior for different components of the newspaper's web, like a lottery widget, using JavaScript",
-        date: 'Jan 2016 - Feb 2018'
-    }
-]
+  {
+    title: 'FULL-STACK DEVELOPER',
+    place: 'LinkApp - Berlin, Germany',
+    description: 'A web page to have all your social media links in one place. Built the app from the ground (frontend, backend and database)',
+    date: '2023'
+  },
+  {
+    title: 'FRONTED DEVELOPER',
+    place: 'Asesoría Técnica Editorial, Madrid, Spain',
+    description: 'ATE is an agency that provides web development services to magazines: Worked in different projects as frontend engineer using HTML, CSS, JS, TypeScript, React and Next among others. Maintenance and customer support.',
+    date: '2020 - 2023'
+  },
+  {
+    title: 'FRONTEND DEVELOPER',
+    place: 'El Independiente - Madrid, Spain',
+    description: 'El Independiente is a popular digital spanish newspaper. Developed fronted features like sliders, timelines, news feed, etc. using: JavaScript, SCSS and HTML.Developed Wordpress internal tools to help journalists using: JavaScript, SCSS, HTML and PHP',
+    date: '2018 - 2020'
+  },
+  {
+    title: 'FRONTEND DEVELOPER',
+    place: 'El Español- Madrid, Spain',
+    description: "El Español is a popular digital spanish newspaper. Created the layout for different pages and widgets, like the Olympics 2016 special or the newspaper's header redesign, using SCSS and HTMLDeveloped behavior for different components of the newspaper's web, like a lottery widget, using JavaScript",
+    date: '2016 - 2018'
+  }
+];
 
 export const Studies: TableContentDetail[] = [
-    {
-        title: 'SELF-TAUGHT / ONLINE COURSES',
-        place: 'Full-Stack Web development  - Berlin, Germany',
-        description: 'Focused on studying latests technologies and frameworks for web development: React, Nodejs, Nestjs, Express, MongoDB with both, JavaScript and TypeScript.',
-        date: 'Jan 2022 - Current'
-    },
-    {
-        title: 'Online Music Production Degree',
-        place: 'PointBlank Music School - London, UK',
-        date: 'Mar 2019 - Dec 2021'
-    },
-    {
-        title: 'Multiplatform Applications Development',
-        place: 'I.E.S. Francisco de Quevedo',
-        description: "Degree in software development: OOP with Java, Android development, Web development with HTML, CSS, JavaScript",
-        date: 'Sep 2012 - Jun 2015'
-    },
-    {
-        title: 'Audiovisual comunication degree',
-        place: 'Universidad Rey Juan Carlos - Madrid, Spain',
-        date: 'Sep 2009 - Jun 2012'
-    },
-]
+  {
+    title: 'CODEWORKS',
+    place: 'Full-Stack Web development  - Berlin, Germany',
+    date: '2023'
+  },
+  {
+    title: 'Online Music Production Degree',
+    place: 'PointBlank Music School - London, UK',
+    date: '2019 - 2021'
+  },
+  {
+    title: 'Multiplatform Applications Development',
+    place: 'I.E.S. Francisco de Quevedo',
+    description: "Degree in software development: OOP with Java, Android development, Web development with HTML, CSS, JavaScript",
+    date: '2012 - 2015'
+  },
+  {
+    title: 'Audiovisual comunication degree',
+    place: 'Universidad Rey Juan Carlos - Madrid, Spain',
+    date: '2009 - 2012'
+  },
+];
